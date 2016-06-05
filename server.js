e778260a348ba504cfd4d021335ec267504c0af8
ipritish <pritishjain2009@gmail.com>
2016-06-05 16:42:23 +0530

    server mysql test

--- server.js
+++ server.js
@@ -4,6 +4,46 @@ var http = require('http');
 var path = require('path');
 var server = http.createServer(app);
 
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'String@1',
+  database : 'inventory'
+});
+
+app.get('/initshow',function(req,res){
+	
+	connection.connect();
+
+	connection.query('SELECT * from shows', function(err, rows, fields) {
+	  if (!err)
+		console.log('The solution is: ', rows);
+	  else
+		console.log('Error while performing Query.');
+	});
+
+	connection.end();
+	
+});
+
+app.get('/initanime',function(req,res){
+	
+	connection.connect();
+
+	connection.query('SELECT * from anime', function(err, rows, fields) {
+	  if (!err)
+		console.log('The solution is: ', rows);
+	  else
+		console.log('Error while performing Query.');
+	});
+
+	connection.end();
+	
+});
+
+
+
 app.get('/test', function(req, res) {
 
   res.send('result');
