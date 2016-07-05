a7cccc860151c7ba9d7c14b42b1bf3406c2f0396
ipritish <pritishjain2009@gmail.com>
2016-07-05 22:00:24 +0530

    db init

--- dbinit.sql
+++ dbinit.sql
@@ -3,4 +3,6 @@ FLUSH PRIVILEGES;
 SHOW DATABASES;
 CREATE DATABASE inventory;
 USE inventory;
+create table animes (id int AUTO_INCREMENT NOT NULL, name varchar(255) NOT NULL, rating double NOT NULL, season int NOT NULL, episodes int NOT NULL, running boolean NOT NULL, air_day varchar(255), my_rating double NOT NULL ,PRIMARY KEY (id));
+create table shows (id int AUTO_INCREMENT NOT NULL, name varchar(255) NOT NULL, rating double NOT NULL, season int NOT NULL, episodes int NOT NULL, running boolean NOT NULL, air_day varchar(255), my_rating double NOT NULL , PRIMARY KEY (id));
 SHOW tables;
