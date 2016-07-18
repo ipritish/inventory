9e9e100a45218865617113664ac867bab3cd707e
ipritish <pritishjain2009@gmail.com>
2016-07-18 12:02:07 +0530

    ui changes

--- public/app/components/anime/addAnimeController.js
+++ public/app/components/anime/addAnimeController.js
@@ -20,10 +20,28 @@ angular.module('inventory')
 
 	$scope.is_runninglabel = "On Air";
 
+	$scope.running = false;
+
 	$scope.addAnime = function(){
 		var data = {};
-		data.name = $scope.name;
-		data.air_day = $scope.air_day;
+
+		if ($scope.name !== undefined && $scope.air_day !== undefined && $scope.season !== undefined && $scope.episodes !== undefined
+		    && $scope.rating !== undefined && $scope.my_rating !== undefined)
+		{
+			var data = {};
+			data.name = $scope.name;
+			data.rating = $scope.rating;
+			data.season = $scope.season;
+			data.episodes = $scope.episodes;
+			data.running = $scope.running;
+			data.air_day = $scope.air_day;
+			data.my_rating = $scope.my_rating;
+			console.log(data);
+		}
+		else
+		{
+			console.log("All Entries are not filled");
+		}
 		//$state.go("anime");
 		/*$http.post("/addanime",data).then(function(response){
 			console.log(response.data);
