71df97cdabc6d4bbcd46e090b5216351a9b6b647
ipritish <pritishjain2009@gmail.com>
2016-07-19 12:36:15 +0530

    data integrity test

--- public/app/components/anime/addAnimeController.js
+++ public/app/components/anime/addAnimeController.js
@@ -27,20 +27,30 @@ angular.module('inventory')
 
 		function correctNonPrintable(val){
 			return val;
-		};
+		}
+
+		function checkDataIntegrity(rating,season,episodes,my_rating){
+			return (!isNaN(rating) && !isNaN(season) && !isNaN(episodes) && !isNaN(my_rating));
+		}
+
 
 		if ($scope.name !== undefined && $scope.air_day !== undefined && $scope.season !== undefined && $scope.episodes !== undefined
 		    && $scope.rating !== undefined && $scope.my_rating !== undefined)
 		{
-			var data = {};
-			data.name = correctNonPrintable($scope.name);
-			data.rating = correctNonPrintable($scope.rating);
-			data.season = $scope.season;
-			data.episodes = $scope.episodes;
-			data.running = $scope.running;
-			data.air_day = $scope.air_day;
-			data.my_rating = $scope.my_rating;
-			console.log(data);
+			if (checkDataIntegrity($scope.rating,$scope.season,$scope.episodes,$scope.my_rating)) {
+				var data = {};
+				data.name = correctNonPrintable($scope.name);
+				data.rating = +$scope.rating;
+				data.season = +$scope.season;
+				data.episodes = +$scope.episodes;
+				data.running = $scope.running;
+				data.air_day = correctNonPrintable($scope.air_day);
+				data.my_rating = $scope.my_rating;
+				console.log(data);
+			}
+			else{
+				console.log("Entries not valid");
+			}
 		}
 		else
 		{
